1b46a8d55f99c5b2cf7e54e5ac0ebf523e1503b6
mia-pan <miapanyuewei@gmail.com>
2019-12-17 17:39:52 -0800

    Done.

---
+++ db/migrate/20191217195618_create_songs.rb
@@ -0,0 +1,11 @@
+class CreateSongs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.boolean :released
+      t.integer :release_year
+
+      t.timestamps
+    end
+  end
+end
